7a7e1a48e279d15e19a9b86e0bcf5d503e109422
aarojasm <aaron.rojas.martinez@gmail.com>
2013-06-10 22:41:01 -0400

    se agrego pagina, para leer las noticias que fueron ingresadas

---
+++ PICCTO/leeNoticia.php
@@ -0,0 +1,30 @@
+<?php
+    include_once "../conexion/conexion.php";
+    $noticia = $_GET['noticia'];
+    $noticia = str_replace("-"," ",$noticia);
+    $sql = "SELECT titulo, cuerpo from piccto.noticia where titulo = '".$noticia."'";
+    $result = mysql_query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+        <link type="text/css" rel="stylesheet" href="/librerias/css/Style.css"/>
+    </head>
+    <body>
+            <?php include "../librerias/include/encabezado.html" ?>
+            <?php include "../librerias/include/menu1.html" ?>
+            <?php include "../librerias/include/menu2.html" ?>
+
+        <div class="page-wrap">   
+            <?php
+                $row = mysql_fetch_array($result);
+                //echo $result;
+                echo "<div class=\"titulo\">".$row['titulo']."</div>";
+                echo "<div class=\"cuerpo\">".$row['cuerpo']."</div>";
+            ?>
+        </div>
+    </body>
+</html>
